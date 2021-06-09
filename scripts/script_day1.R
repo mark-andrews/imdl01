ae4e6ffe814427aaac2888573613c8867368b7d4
Mark Andrews <mjandrews.org@gmail.com>
2021-06-09 16:06:53 +0100

    Update script_day_1.R

--- scripts/script_day1.R
+++ scripts/script_day1.R
@@ -13,7 +13,39 @@ training_df <- affairs_df %>%
   relocate(cheater) %>% 
   as.data.frame()
 
+# data_df (all the data)
+# is split to
+# training_df, test_df (80/20)
+# train() with training_df
+# predict with test_df
 
 # Make our "task" and our "learner" ---------------------------------------
 
-affairsTask <- makeClassifTask(data = training_df, target = 'cheater')
+# make the "task", which is a classification problem
+affairsTask <- makeClassifTask(data = training_df, 
+                               target = 'cheater')
+
+# make the "learner", which is a classifier method, namely logistic regression
+logReg <- makeLearner("classif.logreg", predict.type = 'prob')
+
+# apply the "learner" to the "task", aka train it, aka fit it 
+logRegTrained <- train(logReg, affairsTask)
+
+
+# evaluate predictive performance -----------------------------------------
+
+p <- predict(logRegTrained, newdata = training_df)
+calculateConfusionMatrix(p)
+calculateROCMeasures(p)
+
+# Get model ---------------------------------------------------------------
+
+M <- getLearnerModel(logRegTrained)
+
+
+# Cross validation etc ----------------------------------------------------
+
+kfold_cv <- makeResampleDesc(method = 'RepCV', 
+                             folds = 10, 
+                             reps = 10, 
+                             stratify = TRUE)
